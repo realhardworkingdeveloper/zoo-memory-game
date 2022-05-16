ef6cf73dc4baa38a7102290e14c8cdf9f76e6289
lanxeon <bswapnonil@yahoo.com>
2022-05-17 00:23:02 +0530

    added router and moved landing page to HomePage component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import scrollreveal from 'scrollreveal'
-import Clients from './components/Clients'
-import Footer from './components/Footer'
-import Free from './components/Free'
-import Home from './components/Home'
-import Like from './components/Like'
-import Navbar from './components/Navbar'
-import Release from './components/Release'
-import ScrollToTop from './components/ScrollToTop'
-import SignUp from './components/SignUp'
-import SuperRare from './components/SuperRare'
-import './scss/index.scss'
+import React, { useEffect } from "react";
 
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/home-page/home-page";
+
+import { useTheme } from "./hooks/use-theme";
+
+import "./scss/index.scss";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-
-  const [theme, setTheme] = useState("dark");
-  const changeTheme = () => {
-    theme === "dark" ? setTheme("light") : setTheme("dark")
-  }
-
-  useEffect(() => {
-    const registerAnimations = () => {
-      const sr = scrollreveal({
-        origin: "bottom",
-        distance: "80px",
-        duration: 2000,
-        reset: false
-      });
-      sr.reveal(`
-      nav, .home, .free, .clients, .super-rare, .releases, .like, .signup, footer
-      `, { interval: 500 }
-      );
-    };
-    registerAnimations()
-  }, []);
-
-  window.setTimeout(() => {
-    const home = document.getElementsByClassName("home");
-    home[0].style.transform = "none";
-    const nav = document.getElementsByTagName("nav");
-    nav[0].style.transform = "none";
-  }, 1500)
+  const [theme, changeTheme] = useTheme();
 
   return (
-    <div className='app-container' data-theme={theme}>
-      <ScrollToTop />
+    <div className="app-container" data-theme={theme}>
       <Navbar changeTheme={changeTheme} currentTheme={theme} />
-      <Home />
-      <Free />
-      <Clients />
-      <SuperRare />
-      <Release />
-      <Like />
-      <SignUp />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
-  )
+  );
 }
---
+++ src/hooks/use-theme.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState("dark");
+  const changeTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+
+  return [theme, changeTheme];
+};
